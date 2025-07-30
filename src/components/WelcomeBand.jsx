37fc7f6eb13d399b171a11571a89bbcdf612d291
Furkan Coşar <furkancosar2005@gmail.com>
2025-07-30 19:29:59 +0300

    htofix

--- src/components/WelcomeBand.jsx
+++ src/components/WelcomeBand.jsx
@@ -20,17 +20,19 @@ export default function WelcomeBand() {
   }, []);
 
   return (
-    <div className="w-full overflow-hidden">
-      <h1
-        className="welcome-band text-7xl md:text-8xl lg:text-9xl font-black leading-[1.1] tracking-[-0.08em] whitespace-nowrap bg-white text-white"
-        style={{
-          WebkitTextFillColor: "transparent",
-          WebkitTextStrokeWidth: "2px",
-          WebkitTextStrokeColor: "#202020",
-        }}
-      >
-        WELCOME WELCOME WELCOME WELCOME WELCOME
-      </h1>
+    <div>
+      <div className="w-full overflow-hidden">
+        <div
+          className="welcome-band text-7xl md:text-8xl lg:text-9xl font-black leading-[1.1] tracking-[-0.08em] whitespace-nowrap"
+          style={{
+            WebkitTextStroke: "2px #202020",
+            WebkitTextFillColor: "transparent",
+            textStroke: "2px #202020",
+          }}
+        >
+          WELCOME WELCOME WELCOME WELCOME WELCOME WELCOME WELCOME
+        </div>
+      </div>
     </div>
   );
 }
